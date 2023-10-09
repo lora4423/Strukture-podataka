d0581d2de23a5c00de011db33cc07007177a5d71
lora4423 <loratabak9@gmail.com>
2023-10-09 20:07:57 +0200

    vjezba1

--- Vjezba1LiJ/Vjezba1LiJ/vj.c
+++ Vjezba1LiJ/Vjezba1LiJ/vj.c
@@ -1,10 +1,33 @@
-#include<stdio.h>
 
+
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+typedef struct{
+	char ime[10];
+	char prezime[10];
+	double bodovi;
+}student;
 
 int main()
 {
+	int brojRedova = 0;
+	char charBuffer[1024]="";
+	FILE* filePointer = NULL;
+	
+	filePointer = fopen("studenti.txt", "r");
 
-	printf("Hello World");
+	if (filePointer == NULL){
+		printf("Datoteka nije pronadjena");
+		return 1;
+	}
+	
+	while (!feof(filePointer)) {
+		if (fgets(charBuffer,1024,filePointer) ) {
+			brojRedova++;
+		}
 
+	}
+	printf("%d", brojRedova);
+	fclose(filePointer);
 	return 0;
 }
