ae32bc6d27ca1b87a27aeef1b995573c6ba3fbdc
lora4423 <loratabak9@gmail.com>
2023-10-16 19:58:47 +0200

    pocetak 2 zadatka

---
+++ Vjezba2LiJ/vj2.c/vj2.c/vj2.c
@@ -0,0 +1,23 @@
+﻿/*
+Definirati strukturu osoba (ime, prezime, godina rođenja) i napisati program koji:
+A. dinamički dodaje novi element na početak liste,
+B. ispisuje listu,
+C. dinamički dodaje novi element na kraj liste,
+D. pronalazi element u listi (po prezimenu),
+E. briše određeni element iz liste,
+U zadatku se ne smiju koristiti globalne varijable.*/#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define MAX_LENGTH 50
+
+typedef struct _Person *Position;
+typedef struct _Person 
+{
+	char name[MAX_LENGTH];
+	char surname[MAX_LENGTH];
+	int birthYear;
+
+};
+int main()
+{
+
+}
